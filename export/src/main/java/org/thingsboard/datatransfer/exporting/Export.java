bce551285f6c771a0346a41d8f3c716715278dce
Mitia Shvayka <mitiabmx@gmail.com>
2018-09-13 20:26:14 +0300

    import/export dashboards

--- export/src/main/java/org/thingsboard/datatransfer/exporting/Export.java
+++ export/src/main/java/org/thingsboard/datatransfer/exporting/Export.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.thingsboard.client.tools.RestClient;
 import org.thingsboard.datatransfer.exporting.entities.ExportAssets;
 import org.thingsboard.datatransfer.exporting.entities.ExportCustomers;
+import org.thingsboard.datatransfer.exporting.entities.ExportDashboards;
 import org.thingsboard.datatransfer.exporting.entities.ExportDevices;
 import org.thingsboard.server.common.data.relation.EntityRelation;
 
@@ -56,14 +57,18 @@ public class Export {
 
             log.info("Start exporting...");
 
-            ExportCustomers customers = new ExportCustomers(tbRestClient, mapper, BASE_PATH);
-            customers.getTenantCustomers(relationsArray);
+            /*ExportCustomers customers = new ExportCustomers(tbRestClient, mapper, BASE_PATH);
+            customers.getTenantCustomers(relationsArray);*/
 
             ExportDevices devices = new ExportDevices(tbRestClient, mapper, BASE_PATH);
             devices.getTenantDevices(relationsArray);
 
-            ExportAssets assets = new ExportAssets(tbRestClient, mapper, BASE_PATH);
-            assets.getTenantAssets(relationsArray, telemetryArray);
+           /* ExportAssets assets = new ExportAssets(tbRestClient, mapper, BASE_PATH);
+            assets.getTenantAssets(relationsArray, telemetryArray);*/
+
+            ExportDashboards dashboards = new ExportDashboards(tbRestClient, mapper , BASE_PATH);
+            dashboards.getTenantDashboards(relationsArray);
+
 
             try (BufferedWriter writer = new BufferedWriter(new FileWriter(new File(BASE_PATH + "Relations.json")))) {
                 writer.write(mapper.writeValueAsString(relationsArray));
@@ -72,13 +77,13 @@ public class Export {
                 log.warn("");
             }
 
-            try (BufferedWriter writer = new BufferedWriter(new FileWriter(new File(BASE_PATH + "Telemetry.json")))) {
+            /*try (BufferedWriter writer = new BufferedWriter(new FileWriter(new File(BASE_PATH + "Telemetry.json")))) {
                 writer.write(mapper.writeValueAsString(telemetryArray));
                 writer.close();
             } catch (IOException e) {
                 log.warn("");
             }
-
+*/
             log.info("Ended exporting successfully!");
             EXECUTOR_SERVICE.shutdown();
         } catch (Exception e) {
